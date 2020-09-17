f02b993c0531dcbecd25d06f011475d9d47db8b8
rcomissario <71436817+rcomissario@users.noreply.github.com>
2020-09-17 11:57:22 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Title -->
+    <title>Razo - Radio HTML Template | Home</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="./img/core-img/favicon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <?php include'pages/preloader.php'; ?>
+    <!-- /Preloader -->
+
+    <!-- Top Search Area Start -->
+    <?php include'pages/top_search_area.php'; ?>
+    <!-- Top Search Area End -->
+
+    <!-- Social Share Area Start -->
+    <div class="razo-social-share-area">
+        <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="#" class="pinterest"><i class="fa fa-pinterest"></i></a>
+        <a href="#" class="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="#" class="youtube"><i class="fa fa-youtube-play"></i></a>
+        <a href="#" class="ss-close-btn"><i class="arrow_right"></i></a>
+    </div>
+    <!-- Social Share Area End -->
+
+    <!-- Header Area Start -->
+    <?php include 'pages/header_area.php'; ?>
+    <!-- Header Area End -->
+
+    <!-- Welcome Area Start -->
+   <?php include 'pages/welcome_section.php'; ?>
+    <!-- Welcome Area End -->
+
+    <!-- Music Charts Area Start -->
+    <section class="razo-music-charts-area section-padding-80 bg-overlay bg-img jarallax" style="background-image: url(img/bg-img/31.jpg);">
+        <div class="container">
+            <div class="row">
+                <!-- Section Heading -->
+                <div class="col-sm-6">
+                    <div class="section-heading white">
+                        <h2>Parada musical</h2>
+                    </div>
+                </div>
+                <!-- Show All Button -->
+                <div class="col-sm-6">
+                    <div class="show-all-button mb-50 text-right">
+                        <a href="#" class="btn show-all-btn">Mostrar todas as tendências (10 principais)</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+
+                    <!-- Single Music Chart -->
+                    <div class="single-music-chart d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="100ms">
+                        <!-- Music Content -->
+                        <div class="music-content d-flex align-items-center">
+                            <div class="sl-number">
+                                <h5>1.</h5>
+                            </div>
+                            <div class="music-thumb">
+                                <img src="img/bg-img/25.jpg" alt="">
+                            </div>
+                            <div class="audio-player">
+                                <audio preload="auto" controls>
+                                    <source src="audio/dummy-audio.mp3">
+                                </audio>
+                            </div>
+                            <div class="music-title">
+                                <h5>Way Back Home - <span>RadioDaily Show</span></h5>
+                            </div>
+                        </div>
+                        <!-- Music Price -->
+                        <div class="music-price">
+                            <a href="#" class="btn razo-btn btn-2">$12.99</a>
+                        </div>
+                    </div>
+
+                    <!-- Single Music Chart -->
+                    <div class="single-music-chart d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="300ms">
+                        <!-- Music Content -->
+                        <div class="music-content d-flex align-items-center">
+                            <div class="sl-number">
+                                <h5>2.</h5>
+                            </div>
+                            <div class="music-thumb">
+                                <img src="img/bg-img/26.jpg" alt="">
+                            </div>
+                            <div class="audio-player">
+                                <audio preload="auto" controls>
+                                    <source src="audio/dummy-audio.mp3">
+                                </audio>
+                            </div>
+                            <div class="music-title">
+                                <h5>Goodbye &amp; Good Riddance - <span>Juice World</span></h5>
+                            </div>
+                        </div>
+                        <!-- Music Price -->
+                        <div class="music-price">
+                            <a href="#" class="btn razo-btn btn-2">$10.99</a>
+                        </div>
+                    </div>
+
+                    <!-- Single Music Chart -->
+                    <div class="single-music-chart d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="500ms">
+                        <!-- Music Content -->
+                        <div class="music-content d-flex align-items-center">
+                            <div class="sl-number">
+                                <h5>3.</h5>
+                            </div>
+                            <div class="music-thumb">
+                                <img src="img/bg-img/27.jpg" alt="">
+                            </div>
+                            <div class="audio-player">
+                                <audio preload="auto" controls>
+                                    <source src="audio/dummy-audio.mp3">
+                                </audio>
+                            </div>
+                            <div class="music-title">
+                                <h5>The Greatest Showman - <span>Soundtrack</span></h5>
+                            </div>
+                        </div>
+                        <!-- Music Price -->
+                        <div class="music-price">
+                            <a href="#" class="btn razo-btn btn-2">$10.99</a>
+                        </div>
+                    </div>
+
+                    <!-- Single Music Chart -->
+                    <div class="single-music-chart d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="700ms">
+                        <!-- Music Content -->
+                        <div class="music-content d-flex align-items-center">
+                            <div class="sl-number">
+                                <h5>4.</h5>
+                            </div>
+                            <div class="music-thumb">
+                                <img src="img/bg-img/28.jpg" alt="">
+                            </div>
+                            <div class="audio-player">
+                                <audio preload="auto" controls>
+                                    <source src="audio/dummy-audio.mp3">
+                                </audio>
+                            </div>
+                            <div class="music-title">
+                                <h5>Mamma Mia! Here We Go Again - <span>Soundtrack</span></h5>
+                            </div>
+                        </div>
+                        <!-- Music Price -->
+                        <div class="music-price">
+                            <a href="#" class="btn razo-btn btn-2">$10.99</a>
+                        </div>
+                    </div>
+
+                    <!-- Single Music Chart -->
+                    <div class="single-music-chart d-flex align-items-center justify-content-between wow fadeInUp" data-wow-delay="900ms">
+                        <!-- Music Content -->
+                        <div class="music-content d-flex align-items-center">
+                            <div class="sl-number">
+                                <h5>5.</h5>
+                            </div>
+                            <div class="music-thumb">
+                                <img src="img/bg-img/29.jpg" alt="">
+                            </div>
+                            <div class="audio-player">
+                                <audio preload="auto" controls>
+                                    <source src="audio/dummy-audio.mp3">
+                                </audio>
+                            </div>
+                            <div class="music-title">
+                                <h5>Pray For The Wicked - <span>Panic! At The Disco</span></h5>
+                            </div>
+                        </div>
+                        <!-- Music Price -->
+                        <div class="music-price">
+                            <a href="#" class="btn razo-btn btn-2">$10.99</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Music Charts Area End -->
+
+    <!-- Weekly Sehedule Area Start -->
+    <section class="razo-weekly-schedule-area section-padding-80">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="weekly-schedule-table table-responsive">
+                        <!-- Section Heading -->
+                        <div class="col-12">
+                            <div class="section-heading text-center">
+                                <h2>Horário semanal</h2>
+                            </div>
+                        </div>
+
+                        <table class="table table-borderless">
+                            <thead>
+                                <tr>
+                                    <th scope="col">
+                                        <p>Segunda-feira</p>
+                                    </th>
+                                    <th scope="col">
+                                        <p>Terça-feira</p>
+                                    </th>
+                                    <th scope="col">
+                                        <p>Quarta-feira</p>
+                                    </th>
+                                    <th scope="col">
+                                        <p>Quinta-feira</p>
+                                    </th>
+                                    <th scope="col">
+                                        <p>Sexta-feira</p>
+                                    </th>
+                                    <th scope="col">
+                                        <p>Sabado</p>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">
+                                        <p>As BBC Radio 5 live</p>
+                                        <span>01:00 am - 02:30 am</span>
+                                    </th>
+                                    <td>
+                                        <h5>Andrew Peach Birthday Surprise!</h5>
+                                        <span>13:00 pm - 16:00 pm</span>
+                                    </td>
+                                    <td>
+                                        <h5>As BBC Radio 5 live</h5>
+                                        <span>23:00 pm - 02:00 am</span>
+                                    </td>
+                                    <td>
+                                        <h5>Berkshire Sport: Extra Time</h5>
+                                        <span>01:00 am - 03:30 am</span>
+                                    </td>
+                                    <td>
+                                        <h5>As BBC Radio 5 live</h5>
+                                        <span>01:00 am - 02:30 am</span>
+                                    </td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <th scope="row">
+                                        <p>Berkshire Sport: Extra Time</p>
+                                        <span>05:30 am - 06:30 am</span>
+                                    </th>
+                                    <td></td>
+                                    <td></td>
+                                    <td>
+                                        <h5>Tony Blackburn on BBC Radio</h5>
+                                        <span>03:00 am - 05:00 am</span>
+                                    </td>
+                                    <td></td>
+                                    <td>
+                                        <h5>Stereo Underground</h5>
+                                        <span>03:00 am - 05:00 am</span>
+                                    </td>
+                                </tr>
+
+                                <tr>
+                                    <th scope="row"></th>
+                                    <td>
+                                        <h5>BBC Music Introducing in</h5>
+                                        <span>10:00 am - 12:00 pm</span>
+                                    </td>
+                                    <td>
+                                        <h5>Stereo Underground</h5>
+                                        <span>13:00 pm - 15:00 pm</span>
+                                    </td>
+                                    <td></td>
+                                    <td>
+                                        <h5>Sport on BBC Radio Berkshire</h5>
+                                        <span>07:00 am - 09:30 am</span>
+                                    </td>
+                                    <td></td>
+                                </tr>
+
+                                <tr>
+                                    <th scope="row">
+                                        <p>Andrew Peach Birthday Surprise!</p>
+                                        <span>13:00 pm - 16:00 pm</span>
+                                    </th>
+                                    <td>
+                                    </td>
+                                    <td>
+                                        <h5>BBC Music Introducing in</h5>
+                                        <span>17:00 pm - 20:00 pm</span>
+                                    </td>
+                                    <td>
+                                        <h5>Andrew Peach Birthday Surprise!</h5>
+                                        <span>13:00 pm - 16:00 pm</span>
+                                    </td>
+                                    <td></td>
+                                    <td>
+                                        <h5>Danyl Johnson and Natasha Watts</h5>
+                                        <span>13:00 pm - 16:00 pm</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Weekly Sehedule Area End -->
+
+    <!-- Footer Area Start -->
+   <?php include 'pages/footer_area.php'; ?> 
+    <!-- Footer Area End -->
+
+    <!-- All JS Files -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Popper -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All Plugins -->
+    <script src="js/razo.bundle.js"></script>
+    <!-- Active -->
+    <script src="js/default-assets/active.js"></script>
+
+</body>
+
+</html>
